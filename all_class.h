9198fd0861690ed448eaa3340c0ce3b7ccc4621c
Sanjay <sssv96@gmail.com>
2015-11-12 17:44:14 +0530

    Basic Structure layed out

--- all_class.h
+++ all_class.h
@@ -12,4 +12,17 @@ class NODE
   string label;
   vector<string> in, out;
   bool status;
+public:
+  NODE( string _label, bool _status, vector<string> _in, vector<string> _out )
+  {
+    label = _label;
+    status = _status;
+    in = _in; // try std::move ?
+    out = _out;
+  }
+  vector<NODE> chk_crculr();    // Check for circularities
+  vector<NODE> chk_exist_just();// Check if the justifications exist
+  vector<NODE> chk_contra();    // Check for contradictions
 };
+
+vector<NODE> nodes;
